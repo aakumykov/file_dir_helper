792ed936fceaa44f08b4108b100a8a61ecf1a439
Андрей Кумыков <aakumykov@yandex.ru>
2023-04-03 18:35:27 +0300

    1й

---
+++ file_dir_helper/src/main/java/com/gitlab/aakumykov/file_dir_helper/FileHelper.java
@@ -0,0 +1,45 @@
+package com.gitlab.aakumykov.file_dir_helper;
+
+import android.os.Environment;
+
+import androidx.annotation.NonNull;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileHelper {
+
+    public static List<File> listFilesFromPublicDownloads() {
+
+        final File[] files = publicDownloadsDir().listFiles();
+
+        if (null == files)
+            return new ArrayList<>();
+
+        return Arrays.asList(files);
+    }
+
+    public static List<File> listFilesFromPublicDownloads(final @NonNull String fileExtension) {
+
+        final File[] files = publicDownloadsDir().listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(fileExtension.toLowerCase());
+            }
+        });
+
+        if (null == files)
+            return new ArrayList<>();
+
+        return Arrays.asList(files);
+    }
+
+
+
+    private static File publicDownloadsDir() {
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    }
+}
